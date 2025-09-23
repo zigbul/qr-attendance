bef4af0bd9a789b2601c569c770b81dc028f8459
Zigbul <parfenov.georgiy@bk.ru>
2025-09-23 12:26:53 +0300

    add reset and variables css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import './styles/reset.css';
+import './styles/variables.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
