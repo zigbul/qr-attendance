453db9d5baa2d4844e8cedeb14a5a8eeb431ee78
Zigbul <parfenov.georgiy@bk.ru>
2025-10-05 20:12:18 +0300

    add auth-check when app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import LoginPage from './pages/LoginPage';
 import LessonsPage from './pages/LessonsPage';
 import ScanPage from './pages/ScanPage';
 //import ReportsPage from './pages/ReportsPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import useStore from './store';
+import type { IUser } from './types';
 //import ScanConfirmPage from './pages/ScanConfirmPage';
 
 function App() {
+  const navigate = useNavigate();
+  const [isChecking, setIsChecking] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    try {
+      const response = await fetch('/api/auth-check', {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      if (response.ok === false) {
+        throw new Error(response.statusText);
+      }
+
+      const data = await response.json();
+
+      const currentUser: IUser = {
+        fullName: data.fullname,
+        role: data.role,
+      };
+
+      useStore.getState().setCurrentUser(currentUser);
+
+      if (currentUser.role === 'Teacher') {
+        navigate('/classes');
+      } else {
+        navigate('/scan');
+      }
+    } catch (error) {
+      console.error('Error checking authentication:', error);
+      navigate('/login');
+    } finally {
+      setIsChecking(false);
+    }
+  };
+
+  if (isChecking) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <Routes>
@@ -44,7 +91,7 @@ function App() {
             </ProtectedRoute>
           }
         />*/}
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
